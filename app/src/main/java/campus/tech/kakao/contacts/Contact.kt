b715d428ff5c4027d67ee6025dee153d65a4515a
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-07-02 14:55:47 +0900

    refactor(Contact): var -> val로 수정

--- app/src/main/java/campus/tech/kakao/contacts/Contact.kt
+++ app/src/main/java/campus/tech/kakao/contacts/Contact.kt
@@ -1,10 +1,10 @@
 package campus.tech.kakao.contacts
 
 data class Contact(
-    var name: String,
-    var phoneNumber: String,
-    var email: String,
-    var birthDay: String = "",
-    var gender: String = "",
-    var memo: String = ""
+    val name: String,
+    val phoneNumber: String,
+    val email: String,
+    val birthDay: String = "",
+    val gender: String = "",
+    val memo: String = ""
 ) {}
